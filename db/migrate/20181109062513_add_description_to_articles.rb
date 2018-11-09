950843dd4ad69749d06d483e4921f8403a0df473
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-11-09 14:20:21 +0700

    create articles table model and add validations

---
+++ db/migrate/20181109062513_add_description_to_articles.rb
@@ -0,0 +1,10 @@
+class AddDescriptionToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_033515) do
+ActiveRecord::Schema.define(version: 2018_11_09_062513) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bubbles", force: :cascade do |t|
     t.datetime "created_at", null: false
